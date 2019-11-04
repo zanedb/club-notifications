bbbdc9441d79636297ec89b5a2d06a66ac88b70a
Zane Davis-Barrs <github@zane.sh>
2019-11-03 19:00:25 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+***REMOVED*** Hack Club API
+=================
+
+Our API for [***REMOVED*** Hack Club](https://***REMOVED***/rahs). Currently used for sending notifications.
+
+Setup
+------------
+
+1. Create an `.env` file, using the below template.
+```
+# Find these at https://airtable.com/api
+AIRTABLE_BASE=
+AIRTABLE_API_KEY=
+# Example: if set to 'DEV', will look for Airtable bases called 'Students DEV' and 'Announcements DEV' (note the space!)
+# This is for development purposes, leave blank/undefined in production
+AIRTABLE_BASE_SUFFIX=
+# Find these at https://www.twilio.com/console/project/settings
+TWILIO_ACCOUNT_SID=
+TWILIO_AUTH_TOKEN=
+# Create a messaging service at https://www.twilio.com/console/sms/services
+TWILIO_MESSAGING_SERVICE_SID=
+# Note that you can have multiple (comma-separated) tokens
+AUTH_TOKEN=
+```
+
+2. Install dependencies.
+```
+yarn
+```
+
+3. Start the server.
+```
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,209 @@
+require("dotenv").config();
+
+const validator = require("validator");
+const express = require("express");
+const Airtable = require("airtable");
+const Twilio = require("twilio");
+
+const app = express();
+app.use(express.json());
+const airtableBase = new Airtable(process.env.AIRTABLE_API_KEY).base(
+  process.env.AIRTABLE_BASE
+);
+const twilio = Twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+const TWILIO_COST_PER_TEXT = 0.0075;
+const AIRTABLE_BASE_SUFFIX = process.env.AIRTABLE_BASE_SUFFIX;
+
+const authenticate = token => {
+  const tokens = process.env.AUTH_TOKEN.split(",");
+  return tokens.includes(token);
+};
+
+const base = name =>
+  airtableBase(`${name} ${AIRTABLE_BASE_SUFFIX ? AIRTABLE_BASE_SUFFIX : ""}`);
+
+const getApproxCost = students =>
+  students.filter(student => !validator.isEmpty(student.phone)).length *
+  TWILIO_COST_PER_TEXT;
+
+app.get("/", (req, res) => {
+  res.json({
+    status: 200,
+    message: "hi there fren",
+    url: "/mystery"
+  });
+});
+
+app.get("/mystery", (req, res) => {
+  res.json({
+    status: 200,
+    message: "check back later ;)"
+  });
+});
+
+app.use("/v1/*", (req, res, next) => {
+  // only allow authenticated users to access API
+  if (authenticate(req.body.authToken)) {
+    next();
+  } else {
+    res.status(401).json({ error: "authorization required" });
+  }
+});
+
+// get estimation of notification cost & quantity
+app.get("/v1/notification", (req, res) => {
+  const errors = [];
+  const students = [];
+
+  base("Students")
+    .select({
+      view: "Grid view"
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+          if (record.get("Notifications") === "Enabled") {
+            const contactInfo = record.get("Contact info");
+            const student = {
+              firstName: record.get("First name"),
+              lastName: record.get("Last name")
+            };
+
+            student.email = validator.isEmail(contactInfo) ? contactInfo : "";
+            student.phone = validator.isMobilePhone(contactInfo)
+              ? contactInfo
+              : "";
+
+            students.push(student);
+          }
+        });
+
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          errors.push({ status: 500, error: err.toString() });
+        } else {
+          const approxCost = getApproxCost(students);
+          return res.json({ messagesToSend: students.length, approxCost });
+        }
+      }
+    );
+  if (errors.length > 0) {
+    return res.status(500).json({
+      errors: errors
+    });
+  }
+});
+
+// send notifications
+app.post("/v1/notification", (req, res) => {
+  const subject = req.body.subject;
+  const message = req.body.message;
+
+  const errors = [];
+
+  if (!message) {
+    errors.push({
+      status: 400,
+      message: "missing parameter: message"
+    });
+  }
+  if (!subject) {
+    errors.push({
+      status: 400,
+      message: "missing parameter: subject"
+    });
+  }
+
+  if (message && subject) {
+    const students = [];
+    base("Students")
+      .select({
+        view: "Grid view"
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function(record) {
+            if (record.get("Notifications") === "Enabled") {
+              const contactInfo = record.get("Contact info");
+              const student = {
+                firstName: record.get("First name"),
+                lastName: record.get("Last name")
+              };
+
+              student.email = validator.isEmail(contactInfo) ? contactInfo : "";
+              student.phone = validator.isMobilePhone(contactInfo)
+                ? contactInfo
+                : "";
+
+              students.push(student);
+            }
+          });
+
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            errors.push({ status: 500, error: err.toString() });
+          } else {
+            Promise.all(
+              students.map(student => {
+                if (!validator.isEmpty(student.phone)) {
+                  twilio.messages.create({
+                    to: student.phone,
+                    from: process.env.TWILIO_MESSAGING_SERVICE_SID,
+                    body: message
+                  });
+                }
+                if (!validator.isEmpty(student.email)) {
+                  // TODO: send email
+                  console.log(
+                    `== EMAIL WOULD BE SENT ==\nFrom: noreply@***REMOVED***.com\nTo: ${student.email}\nSubject: ${subject}\nMessage: ${message}\n== END OF EMAIL ==`
+                  );
+                }
+                return;
+              })
+            )
+              .then(messages => {
+                const approxCost = getApproxCost(students);
+                base("Announcements").create(
+                  [
+                    {
+                      fields: {
+                        Message: message,
+                        Subject: subject,
+                        "Messages sent": students.length,
+                        Cost: approxCost
+                      }
+                    }
+                  ],
+                  function(err, records) {
+                    if (err)
+                      errors.push({ status: 500, message: err.toString() });
+                    return;
+                  }
+                );
+                return res.json({ status: 200, message: "messages sent!" });
+              })
+              .catch(err => {
+                errors.push({ status: 500, message: err.toString() });
+              });
+          }
+        }
+      );
+  }
+  if (errors.length > 0) {
+    return res.status(500).json({
+      errors: errors
+    });
+  }
+});
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
