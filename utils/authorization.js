70242099ccb058c2221b460132bfff45ae9317b0
Zane Davis-Barrs <github@zane.sh>
2020-09-13 18:25:38 -0700

    Authorize by either Bearer token or query param

--- utils/authorization.js
+++ utils/authorization.js
@@ -1,17 +1,26 @@
-const { isPopulated, error } = require('./helpers')
+const { isPopulated, error, isEmpty } = require('./helpers')
 
 const { APP_AUTH_TOKEN } = process.env
+const AUTH_TOKENS = APP_AUTH_TOKEN.split(',')
 
 export const authorization = (handler) => (req, res) => {
-  if (isPopulated(req.headers.authorization)) {
-    // validate Bearer token
-    const tokens = APP_AUTH_TOKEN.split(',')
-    if (!tokens.includes(req.headers.authorization.split(' ')[1])) {
-      return error(res, 401, 'permission denied')
-    }
-  } else {
+  if (isEmpty(req.headers.authorization) && isEmpty(req.query.token))
     return error(res, 401, 'authorization required')
-  }
+
+  // authorize by either Bearer token or query param
+  req.authorized = { bearer: false, query: false }
+
+  if (isPopulated(req.headers.authorization))
+    if (AUTH_TOKENS.includes(req.headers.authorization.split(' ')[1])) {
+      req.authorized.bearer = true
+    }
+  if (isPopulated(req.query.token))
+    if (AUTH_TOKENS.includes(req.query.token)) {
+      req.authorized.query = true
+    }
+
+  if (!req.authorized.bearer && !req.authorized.query)
+    return error(res, 401, 'permission denied')
 
   return handler(req, res)
 }
